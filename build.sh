d45bdc3fad3ff7ba2ca232c89b74899ae6c02738
Mireiawen Rose <mir@mireiawen.net>
2021-03-04 17:31:02 +0200

    Change to use Docker Compose

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-# Determine the directory with files
-SCRIPT_DIR="$(realpath "$(dirname "${0}")")"
-
-# Do the build
-docker build "${SCRIPT_DIR}" --tag 'mireiawen/ostoslista-webhook'
